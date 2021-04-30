4ab682107ac81c9c1c94b6a8946fc02882989463
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-04-30 19:54:59 -0400

    Use const for variable that never been reassigned

--- src/homePage.js
+++ src/homePage.js
@@ -1,23 +1,23 @@
 const container = document.getElementById('content');
 
 function insertNavbar() {
-  let navbar = document.createElement('nav');
+  const navbar = document.createElement('nav');
   navbar.setAttribute('class', 'nav nav-pills nav-justified');
   
-  let link1 = document.createElement('a');
-  let textLink1 = document.createTextNode('HOME');
+  const link1 = document.createElement('a');
+  const textLink1 = document.createTextNode('HOME');
   link1.setAttribute('class', 'nav-link text-light');
   link1.setAttribute('href', './index.html');
   link1.appendChild(textLink1);
 
-  let link2 = document.createElement('a');
-  let textLink2 = document.createTextNode('MENU');
+  const link2 = document.createElement('a');
+  const textLink2 = document.createTextNode('MENU');
   link2.setAttribute('class', 'nav-link text-light');
   link2.setAttribute('href', './menu.html');
   link2.appendChild(textLink2);
 
-  let link3 = document.createElement('a');
-  let textLink3 = document.createTextNode('CONTACT');
+  const link3 = document.createElement('a');
+  const textLink3 = document.createTextNode('CONTACT');
   link3.setAttribute('class', 'nav-link text-light');
   link3.setAttribute('href', './home.html');
   link3.appendChild(textLink3);
