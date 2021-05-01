df5a7932e48baf669d1a21df1668bf539467d1c7
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-05-01 18:36:01 -0400

    Fixing linter

--- src/contact.js
+++ src/contact.js
@@ -7,11 +7,11 @@ function insertForm() {
   const myForm = document.createElement('form');
 
   const firstDiv = document.createElement('div');
-  firstDiv.setAttribute('class', 'mb-3')
+  firstDiv.setAttribute('class', 'mb-3');
   const firstLabel = document.createElement('label');
   firstLabel.setAttribute('for', 'email');
-  firstLabel.setAttribute('class','form-label');
-  firstLabel.innerHTML = "Enter your email";
+  firstLabel.setAttribute('class', 'form-label');
+  firstLabel.innerHTML = 'Enter your email';
   const firstInput = document.createElement('input');
   firstInput.setAttribute('id', 'email');
   firstInput.setAttribute('class', 'form-control');
@@ -23,8 +23,8 @@ function insertForm() {
   secondDiv.setAttribute('class', 'mb-3');
   const secondLabel = document.createElement('label');
   secondLabel.setAttribute('for', 'message');
-  secondLabel.setAttribute('class','form-label');
-  secondLabel.innerHTML = "Leave your message";
+  secondLabel.setAttribute('class', 'form-label');
+  secondLabel.innerHTML = 'Leave your messag';
   const secondInput = document.createElement('textarea');
   secondInput.setAttribute('id', 'message');
   secondInput.setAttribute('class', 'form-control');
