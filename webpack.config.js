d3a00e354a84304926107899cc9fedf19af49eb3
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-04-30 21:26:39 -0400

    Add default export

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
-const path = require('path');
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+import { resolve } from 'path';
+
+export const entry = './src/index.js';
+export const output = {
+  filename: 'main.js',
+  path: resolve(__dirname, 'dist'),
+};
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+  ],
 };
