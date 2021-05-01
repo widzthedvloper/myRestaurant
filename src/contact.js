7ed4d48aa0428460684ba2f3ee0f75210fbd41ab
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-05-01 18:19:38 -0400

    Create insertForm function

---
+++ src/contact.js
@@ -0,0 +1,43 @@
+const container = document.getElementById('content');
+
+function insertForm() {
+  const contentDiv = document.createElement('div');
+  contentDiv.setAttribute('class', 'container text-white content-div');
+
+  const myForm = document.createElement('form');
+
+  const firstDiv = document.createElement('div');
+  const firstLabel = document.createElement('label');
+  firstLabel.setAttribute('for', 'email');
+  firstLabel.setAttribute('class','form-label');
+  firstLabel.innerHTML = "Enter your email";
+  const firstInput = document.createElement('input');
+  firstInput.setAttribute('id', 'email');
+  firstInput.setAttribute('class', 'form-control');
+
+  firstDiv.appendChild(firstLabel);
+  firstDiv.appendChild(firstInput);
+
+  const secondDiv = document.createElement('div');
+  const secondLabel = document.createElement('label');
+  secondLabel.setAttribute('for', 'message');
+  secondLabel.setAttribute('class','form-label');
+  secondLabel.innerHTML = "Leave your message";
+  const secondInput = document.createElement('textarea');
+  secondInput.setAttribute('id', 'message');
+  secondInput.setAttribute('class', 'form-control');
+
+  secondDiv.appendChild(secondLabel);
+  secondDiv.appendChild(secondInput);
+
+  const button = document.createElement('button');
+  button.innerHTML = 'SEND MESSAGE';
+
+  myForm.appendChild(firstDiv);
+  myForm.appendChild(secondDiv);
+  myForm.appendChild(button);
+
+  container.appendChild(myForm);
+}
+
+export default insertForm;
