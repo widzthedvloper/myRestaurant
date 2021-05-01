dde469511d40a4a66461ecd52eb14853aff36592
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-05-01 15:10:14 -0400

    Import the menu function to the index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import insertNavbar from './homePage';
 import homeContent from './homeContent';
+import insertMenu from './menu';
 import './style.css';
 
 insertNavbar();
 homeContent();
+insertMenu();
